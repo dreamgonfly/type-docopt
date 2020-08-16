5fe711addf1bddfede90cc413bf04d90ac377d40
Yongrae Jo <dreamgonfly@gmail.com>
2020-08-16 23:29:54 +0900

    chore: Misc changes

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 from type_docopt import __version__
+
+
+class PyTestCommand(TestCommand):
+    """ Command to run unit py.test unit tests
+    """
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run(self):
+        import pytest
+
+        rcode = pytest.main(self.test_args)
+        sys.exit(rcode)
 
 
 setup(
@@ -13,15 +31,15 @@ setup(
     keywords='option arguments parsing optparse argparse getopt',
     url='http://docopt.org',
     py_modules=['type_docopt'],
-    long_description=open('README.rst').read(),
+    long_description=open("README.md").read(),
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Topic :: Utilities',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
     ],
+    tests_require=["pytest"],
+    cmdclass={"test": PyTestCommand},
 )
