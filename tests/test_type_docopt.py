ec46ad1904d220cf3454e85f12a0022b361461ae
Yongrae Jo <dreamgonfly@gmail.com>
2020-08-17 13:03:15 +0900

    test: Add test_type_docopt

--- tests/test_type_docopt.py
+++ tests/test_type_docopt.py
@@ -1,102 +1,58 @@
-from __future__ import with_statement
+from type_docopt import docopt
 
-from type_docopt import (
-    docopt,
-    DocoptExit,
-    DocoptLanguageError,
-    Option,
-    Argument,
-    Command,
-    AnyOptions,
-    Required,
-    Optional,
-    Either,
-    OneOrMore,
-    parse_argv,
-    parse_pattern,  # parse_defaults,
-    printable_usage,
-    formal_usage,
-    TokenStream,
-)
 from pytest import raises
 
 
-def test_types():
-    doc = """Usage: prog [options]\n
-Options:\n  -d --data=<data>    Input data [type: float]"""
-    a = docopt(doc, "--data=0.1")
-    assert a == {"--data": 0.1}
-    doc = """Usage: prog [--data=<data>]\n
-Options:\n  -d --data=<data>    Input data [default: 10] [type: int]
-"""
-    a = docopt(doc, "")
-    assert a == {"--data": 10}
-    doc = """Usage: prog [--data=<data>]\n
-    Options:\n  -d --data=<data>    Input data [default: 10] [type: int]
-    """
-    a = docopt(doc, "--data=30")
-    assert a == {"--data": 30}
-
-
-def test_user_defined_types():
-    doc = """Usage: prog [--data=<data>]\n
-                     Options:\n  -d --data=<data>    Input data [type: Foo]
-                  """
-
-    class Foo:
+def test_custom_types():
+    class CustomType:
         def __init__(self, number):
             self.number = int(number)
-            assert self.number < 10
+            if self.number >= 10:
+                raise ValueError
 
         def __eq__(self, other):
             return self.number == other.number
 
-    a = docopt(doc, "--data=1", types={"Foo": Foo})
-    assert a == {"--data": Foo("1")}
+    assert docopt(
+        "Usage: prog [--foo=<foo>]\n"
+        "Options: -f --foo=<foo>  Lorem ipsum dolor [type: CustomType]",
+        argv="--foo=1",
+        types={"CustomType": CustomType},
+    ) == {"--foo": CustomType("1")}
 
-    with raises(AssertionError):
-        docopt(doc, "--data=20", types={"Foo": Foo})
+    assert docopt(
+        "Usage: prog [--foo=<foo>]\n"
+        "Options: -f --foo=<foo>  Lorem ipsum dolor [type: CustomType]",
+        argv="",
+        types={"CustomType": CustomType},
+    ) == {"--foo": None}
 
-    doc = """Usage: prog [--data=<data>]\n
-                         Options:\n  -d --data=<data>    Input data [type: Foo] [default: 5]
-                      """
-    a = docopt(doc, "", types={"Foo": Foo})
-    assert a == {"--data": Foo("5")}
+    with raises(ValueError):
+        docopt(
+            "Usage: prog [--foo=<foo>]\n"
+            "Options: -f --foo=<foo>  Lorem ipsum dolor [type: CustomType]",
+            argv="--foo=20",
+            types={"CustomType": CustomType},
+        )
 
-    doc = """Usage: prog [options]\n
-    Options:\n  -d --data <data>  Input data [type: Foo]
-    """
-    a = docopt(doc, "", types={"Foo": Foo})
-    assert a == {"--data": None}
+    assert docopt(
+        "Usage: prog [--foo=<foo>]\n"
+        "Options: -f --foo=<foo>  Lorem ipsum dolor [type: CustomType] [default: 5]",
+        "",
+        types={"CustomType": CustomType},
+    ) == {"--foo": CustomType("5")}
 
 
 def test_choices():
-    doc = """Usage: prog [--data=<data>]\n
-                 Options:\n  -d --data=<data>    Input data [choices: A B C]
-              """
-    a = docopt(doc, "--data=A")
-    assert a == {"--data": "A"}
-    doc = """Usage: prog [--data=<data>]\n
-                     Options:\n  -d --data=<data>    Input data [choices: A B C] [default: C]
-                  """
-    a = docopt(doc, "")
-    assert a == {"--data": "C"}
+    assert docopt(
+        "Usage: prog [--foo=<foo>]\n"
+        "Options: -f --foo=<foo>  Lorem ipsum dolor [choices: A B C]",
+        "--foo=A",
+    ) == {"--foo": "A"}
 
-    doc = """Usage: prog [--data=<data>]\n
-             Options:\n  -d --data=<data>    Input data [choices: A B C]
-          """
-    with raises(AssertionError):
-        docopt(doc, "--data=D")
-
-
-# https://github.com/docopt/docopt/issues/259
-def test_blank_line_between_options():
-    doc = """Usage: prog [options]
-
-Options:
-  -d --data=<data>    Input data [default: 10] [type: int]
-
-  -i --input=<input>    Input [default: 0.1] [type: float]
-"""
-    a = docopt(doc, "")
-    assert a == {"--data": 10, "--input": 0.1}
+    with raises(ValueError):
+        docopt(
+            "Usage: prog [--foo=<foo>]\n"
+            "Options: -f --foo=<foo>  Lorem ipsum dolor [choices: A B C]",
+            "--foo=D",
+        )
